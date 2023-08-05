c21ae17e4df05355d3de55f0038ae10c03ffb008
Supachote Suchotikokul <ong.develop@gmail.com>
2023-08-05 21:36:02 +0700

    refactor: change comment pattern

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -5,21 +5,21 @@ vim.cmd('filetype plugin indent on') -- Load filetype-specific indent files
 vim.opt.clipboard = unnamedplus -- Use system clipboard
 vim.opt.mouse = a -- Allow mouse
 
--- ##### UI #####
+-- UI
 vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true
 vim.opt.cursorline = true -- Highlight current line
 vim.opt.splitbelow = true -- Open new vertical split bottom
 vim.opt.splitright = true -- Open new horizontal split bottom
 
--- ##### Tab #####
+-- Tab
 local indent = 4
 vim.opt.tabstop = indent
 vim.opt.softtabstop = indent
 vim.opt.shiftwidth = indent
 vim.opt.expandtab = true
 
--- ##### Search #####
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true -- Case sensitive if an uppercase is entered
 vim.opt.incsearch = true -- Search as characters are entered
