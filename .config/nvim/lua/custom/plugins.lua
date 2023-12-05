6236cb3c9ff819f8c5604becca7855959226327a
Supachote Suchotikokul <ong.develop@gmail.com>
2023-12-05 20:23:12 +0700

    [CONFIG][NEOVIM][plugin: add null-ls plugin to support format and lint]

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -2,6 +2,12 @@ local plugins = {
   -- In order to modify the `lspconfig` configuration:
   {
     "neovim/nvim-lspconfig",
+     dependencies = {
+       "jose-elias-alvarez/null-ls.nvim",
+       config = function()
+         require "custom.configs.null-ls"
+       end,
+     },
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
